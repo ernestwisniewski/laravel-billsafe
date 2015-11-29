48833b485f6fb20f46e4803f7acddafe088c7a6b
Ernest Wisniewski <ernest@netkata.com>
2015-11-29 15:53:10 +0100

    Logger facade

--- src/Billsafe/Billsafe.php
+++ src/Billsafe/Billsafe.php
@@ -1,7 +1,11 @@
 <?php namespace ErnestWisniewski\LaravelBillsafe;
 
-use Illuminate\Support\Facades\URL;
 use ErnestWisniewski\Billsafe\Sdk;
+use ErnestWisniewski\Billsafe\Logger;
+use ErnestWisniewski\Billsafe\Logger\LoggerEcho;
+use ErnestWisniewski\Billsafe\Logger\LoggerFile;
+use ErnestWisniewski\Billsafe\Logger\LoggerMail;
+use ErnestWisniewski\Billsafe\Logger\LoggerNull;
 
 class Billsafe {
 
@@ -10,6 +14,26 @@ class Billsafe {
      */
     public function sdk()
     {
+        $config = config_path('billsafe.php');
+
+        if (\File::exists($config))
+        {
+            return new Sdk($config);
+        }
+
         return new Sdk();
+    }
+
+    /**
+     * @param $type
+     * @param string $attr
+     * @return mixed | Logger Object
+     */
+    public function logger($type, $attr = '')
+    {
+        $class = Logger::class;
+        $type = $class . '\\' . $type;
+
+        return new $type($attr);
     }
 }
